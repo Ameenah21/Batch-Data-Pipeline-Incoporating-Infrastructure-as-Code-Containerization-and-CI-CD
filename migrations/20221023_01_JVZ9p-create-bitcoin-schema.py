30c24dff5bde0514d360c8596da13627e34b2ffb
Ameenah21 <aminatlawal21@gmail.com>
2023-07-11 11:52:46 +0100

    creating project files

--- migrations/20221023_01_JVZ9p-create-bitcoin-schema.py
+++ migrations/20221023_01_JVZ9p-create-bitcoin-schema.py
@@ -1,11 +1,12 @@
 """
-create bitcoin schema
+Create user_behavior_metric table
 """
-
-from typing import Any
 
 from yoyo import step
 
-__depends__: Any = {}
-
-steps = [step("CREATE SCHEMA bitcoin", "DROP SCHEMA bitcoin")]
+steps = [
+    step(
+        "CREATE TABLE public.user_behavior_metric ( customerid INTEGER, amount_spent DECIMAL(18, 5), review_score INTEGER, review_count INTEGER, insert_date DATE )",
+        "DROP TABLE public.user_behavior_metric",
+    )
+]
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -5,6 +5,13 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+## AWS S3 bucket details
+variable "bucket_prefix" {
+  description = "Bucket prefix for our datalake"
+  type        = string
+  default     = "sde-data-lake-"
+}
+
 ## Key to allow connection to our EC2 instance
 variable "key_name" {
   description = "EC2 key name"
@@ -12,23 +19,34 @@ variable "key_name" {
   default     = "sde-key"
 }
 
-## EC2 instance type
+## AWS EMR node type and auto termination time (EMR is expensive!)
 variable "instance_type" {
   description = "Instance type for EMR and EC2"
   type        = string
   default     = "m4.xlarge"
 }
 
-## Alert email receiver
-variable "alert_email_id" {
-  description = "Email id to send alerts to "
-  type        = string
-  default     = "you-name@some-domain.com"
+variable "auto_termination_timeoff" {
+  description = "Auto EMR termination time(in idle seconds)"
+  type        = number
+  default     = 14400 # 4 hours
 }
 
-## Your repository url
-variable "repo_url" {
-  description = "Repository url to clone into production machine"
+## AWS Redshift credentials and node type
+variable "redshift_user" {
+  description = "AWS user name for Redshift"
   type        = string
-  default     = "https://github.com/josephmachado/data_engineering_project_template.git"
+  default     = "sde_user"
+}
+
+variable "redshift_password" {
+  description = "AWS password for Redshift"
+  type        = string
+  default     = "sdeP0ssword0987"
+}
+
+variable "redshift_node_type" {
+  description = "AWS Redshift node  type"
+  type        = string
+  default     = "dc2.large"
 }
